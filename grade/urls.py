6a19f90cbf67a4f411abc92febd83ab962617377
Matheus Costa <matheus20costa@hotmail.com>
2017-10-11 12:37:54 -0300

    Atualização corrigindo bugs

--- grade/urls.py
+++ grade/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 
 urlpatterns = [
-url(r'^d', views.disc_new, name='disc_new'),
-url(r'^p', views.prof_new, name='prof_new'),
-url(r'^$', views.aula_new, name='aula_new'),
+    url(r'^novadisciplina', views.disc_new, name='disc_new'),
+    url(r'^disciplinas', views.disc_list, name='disc_list'),
+    url(r'^novoprofessor', views.prof_new, name='prof_new'),
 
 ]
